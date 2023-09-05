f5faae7379133af503413dfdfd3ba6b578bb78e5
MuhammadZaidqureshi <141563320+MuhammadZaidqureshi@users.noreply.github.com>
2023-09-05 19:49:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+(function(){
+});
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+
+
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + names);
+  })();
+
+
+
+
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + names);
+  })();
