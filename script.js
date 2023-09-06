933a522940b1ca4535712ca10eec1454a4568fcb
MuhammadZaidqureshi <141563320+MuhammadZaidqureshi@users.noreply.github.com>
2023-09-06 15:31:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,104 @@
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
 
-
+var name1= "Yaakov";
 var speakWord = "Hello";
 (function speak(name) {
-    console.log(speakWord + " " + names);
+    console.log(speakWord + " " + name1);
+  })();
+var name1 = "John";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Jen";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Jason";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Pauk";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Frank";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Larry";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Paula";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Laura";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+    var name1 = "Jim";
+var speakWord = "Hello";
+(function speak(name) {
+    console.log(speakWord + " " + name1);
   })();
 
-
-
-
+var name1 = "Yaakov";
   var speakWord = "Good Bye";
   (function speak(name) {
-    console.log(speakWord + " " + names);
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "John";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Jen";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Jason";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Paul";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Frank";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Larry";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Paula";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Laura";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
+  })();
+  var name1 = "Jim";
+  var speakWord = "Good Bye";
+  (function speak(name) {
+    console.log(speakWord + " " + name1);
   })();
